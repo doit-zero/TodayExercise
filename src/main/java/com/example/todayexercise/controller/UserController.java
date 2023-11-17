7f3879b54851e92b785081ed86b670e54981d760
doit-zero <131241458+doit-zero@users.noreply.github.com>
2023-11-17 13:39:09 +0900

    feat : Generic을 활용한 커스텀 공통 응담폼
    - Generic을 활용하여 커스텀 공통 응답폼을 만들었습니다.
    - 응답은 항상 200이며 그 에러 상태와 내용은 응답값 안에 있습니다. 이렇게 만든 이유는 프론트에서 API 테스트시, 먼저 요청이 서버에 제대로 가는지 확인하기 위함입니다.

--- src/main/java/com/example/todayexercise/controller/UserController.java
+++ src/main/java/com/example/todayexercise/controller/UserController.java
@@ -1,14 +1,23 @@
 package com.example.todayexercise.controller;
 
+import com.example.todayexercise.common.CommonResponse;
 import com.example.todayexercise.dto.request.Login;
 import com.example.todayexercise.dto.request.SingUp;
+import com.example.todayexercise.entity.User;
+import com.example.todayexercise.exception.domain.CommonErrorCode;
+import com.example.todayexercise.exception.domain.CommonException;
+import com.example.todayexercise.exception.domain.User.UserErrorCode;
+import com.example.todayexercise.exception.domain.User.UserException;
 import com.example.todayexercise.service.UserService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.RedisTemplate;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 @Tag(name = "유저", description = "유저 관련 API")
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api")
@@ -17,22 +26,20 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/signup")
-    public String signup(@RequestBody SingUp signup) {
-        return userService.signup(signup);
+    public CommonResponse<String> signup(@RequestBody SingUp signup) {
+        return CommonResponse.success(userService.signup(signup));
     }
 
     @PostMapping("/login")
-    public String login(@RequestBody Login login , HttpSession session) {
-        return userService.login(login,session);
+    public CommonResponse<String> login(@RequestBody Login login , HttpSession session) {
+        return CommonResponse.success(userService.login(login,session));
     }
 
+    @PostMapping("/logout")
+    public CommonResponse<String> logout(HttpSession session, @AuthenticationPrincipal User user) {
+        if(session == null) throw new CommonException(CommonErrorCode.FAIL_TO_SAVE);
+        return CommonResponse.success(userService.logout(session,user));
+    }
 
-
-    //레디스 연동 확인용
-//    @PostMapping("/test")
-//    public String testRedis(@RequestBody Login login){
-//        redisTemplate.opsForValue().set(login.getEmail(),login.getPassword());
-//        return "dd";
-//    }
 
 }
--- src/main/java/com/example/todayexercise/service/UserService.java
+++ src/main/java/com/example/todayexercise/service/UserService.java
@@ -3,19 +3,18 @@ package com.example.todayexercise.service;
 import com.example.todayexercise.dto.request.Login;
 import com.example.todayexercise.dto.request.SingUp;
 import com.example.todayexercise.entity.User;
-import com.example.todayexercise.exception.domain.CommonErrorCode;
-import com.example.todayexercise.exception.domain.CommonException;
 import com.example.todayexercise.exception.domain.User.UserErrorCode;
 import com.example.todayexercise.exception.domain.User.UserException;
 import com.example.todayexercise.repository.User.UserRepository;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
-
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class UserService {
@@ -23,9 +22,8 @@ public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
+    @Transactional
     public String signup(SingUp signup) {
-        // TODO 이메일 & 닉네임 중복 검사
-
         User user = User.builder()
                 .email(signup.getEmail())
                 .password(passwordEncoder.encode(signup.getPassword()))
@@ -33,11 +31,9 @@ public class UserService {
                 .nickName(signup.getNickName())
                 .isDeleted(false)
                 .build();
-
         userRepository.save(user);
         return "회원가입 완료";
     }
-
 
     @Transactional(readOnly = true)
     public String login(Login login, HttpSession session) {
@@ -48,8 +44,10 @@ public class UserService {
         return session.getId();
     }
 
-    public String logout(HttpSession session) {
-        session.removeAttribute("user");
+    public String logout(HttpSession session,User user) {
+        if(user == null) throw new UserException(UserErrorCode.NOT_EXIST_USER);
+        log.info("로그아웃된 사용자 이메일 : {}", user.getEmail());
+        session.invalidate();
         return "로그아웃 되었습니다.";
     }
 }

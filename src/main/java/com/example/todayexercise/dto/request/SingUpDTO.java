5ee3b3856d3d2c272cb661928f3bf70fffdd9add
doit-zero <131241458+doit-zero@users.noreply.github.com>
2023-12-07 21:09:06 +0900

    feat : cors 설정 추가
    - cors 설정을 추가하였습니다.

--- src/main/java/com/example/todayexercise/dto/request/SingUpDTO.java
+++ src/main/java/com/example/todayexercise/dto/request/SingUpDTO.java
@@ -2,7 +2,6 @@ package com.example.todayexercise.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
 public class SingUpDTO {

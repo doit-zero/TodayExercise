4cdd5e06c38223871621879c2b807f46b0a03dd3
doit-zero <131241458+doit-zero@users.noreply.github.com>
2023-12-19 00:51:00 +0900

    refactor : LocalDateTime 한국시간
    LocalDateTime 한국시간으로 변경하였습니다.

--- src/main/java/com/example/todayexercise/TodayExerciseApplication.java
+++ src/main/java/com/example/todayexercise/TodayExerciseApplication.java
@@ -1,11 +1,17 @@
 package com.example.todayexercise;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class TodayExerciseApplication {
-
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
     public static void main(String[] args) {
         SpringApplication.run(TodayExerciseApplication.class, args);
     }
